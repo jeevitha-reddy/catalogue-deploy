96bb4fbdea72d38beb584576014bf587fb258d46
jeevitha <jeevithareddy24@gmail.com>
2024-06-19 08:42:28 +0530

    version

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "devrobo-state-dev"
+key    = "catalogue"
+region = "us-east-1"
+dynamodb_table = "devrobo-locking-dev"
---
+++ terraform/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "devrobo-state-prod"
+key    = "catalogue"
+region = "us-east-1"
+dynamodb_table = "devrobo-locking-prod"
